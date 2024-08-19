25f5d0f7860862e26e404842d60fae904c387cde
Nusra Thameem <fathynus11@gmail.com>
2024-08-19 19:23:36 +0530

    Initial commit with project structure

---
+++ restaurant-app/src/test/java/com/example/restaurant_app/RestaurantAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurant_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
