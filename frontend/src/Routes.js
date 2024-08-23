3d3dcc49fd7085098b550cc0ed3869978f840a56
Nusra Thameem <fathynus11@gmail.com>
2024-08-23 15:28:35 +0530

    organized and updated the HomePage

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomePage from './components/HomePage';
-import Menu from './components/Menu';
+import HomePage from './components/HomePage/HomePage';
+
+
+// import Menu from './components/Menu';
 
 const RoutesComponent = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/menu" element={<Menu />} />
-        {/* Add more routes here */}
-      </Routes>
+            </Routes>
     </Router>
   );
 };
---
+++ frontend/src/components/HomePage/HomePage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './HomePage.css';
+
+class HomePage extends React.Component {
+  render() {
+    return (
+      <main className="homepage">
+        <section className="intro">
+       
+          <h2>Welcome to ABC Restaurant</h2>
+          <p>
+            Experience the best dining experience with our delicious meals and exceptional service.
+          </p>
+        </section>
+      
+      </main>
+    );
+  }
+
+}
+
+export default HomePage;
