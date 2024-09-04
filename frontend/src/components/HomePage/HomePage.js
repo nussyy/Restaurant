7cde2af0380e2d7e4a7d7a81c0c57d9496961d32
Nusra Thameem <fathynus11@gmail.com>
2024-09-04 23:56:08 +0530

    home pages changes

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,14 +1,27 @@
 
 import React from 'react';
+import restauranthome from '../../assets/images/restauranthome.jpeg';
+
 
 const HomePage = () => {
     return (
-        <div>
-            <h1>Welcome to ABC Restaurant</h1>
-            <p>This is the home page.</p>
-        </div>
+        <div className="homepage-container">
+        <header className="homepage-header">
+          <h1>Your Heading Here</h1>
+          <p className="homepage-subparagraph">Your subparagraph text goes here. This is a brief description or additional information.</p>
+        </header>
+        <section className="homepage-content">
+          
+
+        
+          <div className="image-only">
+          <img src={restauranthome} alt="Just an Image" />
+            <button className="reservation-button">Make a Reservation</button>
+          </div>
+        </section>
+      </div>
     );
-};
+  }
 
 export default HomePage;
 
