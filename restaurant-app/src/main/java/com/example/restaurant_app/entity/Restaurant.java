0683a3b9d60075d1e89bb68ca7a2cad54b3bc41f
Nusra Thameem <fathynus11@gmail.com>
2024-09-05 23:59:51 +0530

    added restaurant and reservation entity class

--- restaurant-app/src/main/java/com/example/restaurant_app/entity/Restaurant.java
+++ restaurant-app/src/main/java/com/example/restaurant_app/entity/Restaurant.java
@@ -1,5 +1,38 @@
 package com.example.restaurant_app.entity;
 
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name = "Restaurants")
 public class Restaurant {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long restaurantId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String location;
+
+    @Column
+    private String phoneNumber;
+
+    @Column
+    private String email;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+
 }
